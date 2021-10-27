f71b5ebdd802ebdf6b49e8e5ce1f8d904468e1ba
mferrara625 <mferrara401@yahoo.com>
2021-10-26 23:53:15 -0400

    updated to track created employees and display on main page

--- src/main/java/com/careerdevs/employeeSystem/EmployeeController.java
+++ src/main/java/com/careerdevs/employeeSystem/EmployeeController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 
@@ -12,10 +14,15 @@ import java.util.concurrent.atomic.AtomicLong;
 public class EmployeeController {
 
     private final AtomicLong counter = new AtomicLong();
+    private List<Employee> employeeList = new ArrayList<>();
 
     @GetMapping("/")
     public String rootRoute(){
-        return "Welcome To Employee System";
+        String output = "";
+        for(Employee employee : employeeList){
+            output += "\n" + employee.getFirstName() + " " + employee.getLastName() + ",";
+        }
+        return "Welcome To Employee System:\n\n" + output;
     }
 
     @GetMapping("/dummyEmployee")
@@ -25,7 +32,9 @@ public class EmployeeController {
 
     @GetMapping("/createEmployee")
     public Employee createEmployee(@RequestParam(value = "firstName", defaultValue = "Valued") String firstName, @RequestParam(value = "lastName", defaultValue = "Employee") String lastName){
-        return new Employee(counter.incrementAndGet(), firstName, lastName);
+        Employee employee = new Employee(counter.incrementAndGet(), firstName, lastName);
+        employeeList.add(employee);
+        return employee;
     }
 
     @GetMapping("/employeeLastName/{lastName}")
